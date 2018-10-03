1d6a1b26433c4dcd16b83198c2a0a5ebc59a18a3
Zhi Tan <zhi.tan@ri.cmu.edu>
2018-10-03 17:57:11 -0400

    Added directory resolve function

--- alloy/ros/basic.py
+++ alloy/ros/basic.py
@@ -1,8 +1,11 @@
 import std_msgs.msg
-
+import os
+from rospkg import RosPack
+import rospy
 
 __all__ = [
-    'create_ros_header'
+    'create_ros_header',
+    'resolve_res_path'
 ]
 
 def create_ros_header(rospy, frame=""):
@@ -25,3 +28,51 @@ def create_ros_header(rospy, frame=""):
     msg.frame_id = frame
 
     return msg
+
+def resolve_res_path(path, package_name=None, res_path=None):
+    """
+    Follow a list of rules to find this path.
+    (1) If the path exist and it's a file, return True, path
+    (2) package_name_dir/path
+    (3) Extra filename and apply package_name/res/file_name
+
+    parameters
+    ----------
+    path: string
+        the complete path or just the filename
+    package_name: string (optional)  
+        ROS package name
+
+    res_path: string (optional)  
+        resource directory in ROS package
+
+    returns
+    -------
+    string
+        If the path exist, the actual path. None if unresolvable.
+    """
+
+    #Rule (1)
+    if os.path.isfile(path):
+        return path
+
+    #Rule (2)
+    if package_name:
+        #try to find the ros package
+        rp = RosPack()
+        try:
+            dirpath = rp.get_path(package_name)
+        except ResourceNotFound as err:
+            rospy.logwarn('unable find given rospackage in "resolve_path"')
+            return None
+        if res_path is None:
+            res_path = 'res'
+        filepath = os.path.join(dirpath,res_path,path)
+        if os.path.isfile(filepath):
+            return filepath
+        else:
+            return None
+    else:
+        return None
+
+       
