c11731f41dc960ef85f401d1002ae9aa7da2a801
zhi <zhi@zhi.fyi>
2019-04-04 11:52:53 -0400

    added two way image convertion for roslibpy

--- alloy/ros/roslibpy.py
+++ alloy/ros/roslibpy.py
@@ -5,36 +5,60 @@ import numpy as np
 import base64
 import matplotlib.pyplot as plt
 
+import logging
+logging.basicConfig()
+
+def _get_encoding_information(encoding):
+
+    if encoding == 'bgra8':
+        return 4, np.uint8
+    elif encoding == 'bgr8':
+        return 3, np.uint8
+    elif encoding == 'mono8':
+        return 1, np.uint8
+
 def image_to_numpy(image_msg_dict, desire_encoding='passthrough'):
     """
     Convert sensor_msgs/Image (in dict form) to a numpy array
     only works with bgra8
     """
-    if image_msg_dict['encoding'] != 'bgra8':
-        raise NotImplementedError("Unable to convert other image besides bgra8")
+    #if image_msg_dict['encoding'] != 'bgra8':
+    #    raise NotImplementedError("Unable to convert other image besides bgra8")
+
+
+    num_channel, dtype = _get_encoding_information(image_msg_dict['encoding'])
 
     buffer = base64.b64decode(image_msg_dict['data'])
-    data_arr = np.frombuffer(buffer, np.uint8)
-    data_arr = np.reshape(data_arr,(image_msg_dict['height'],image_msg_dict['width'],4))
+    data_arr = np.frombuffer(buffer, dtype)
+    data_arr = np.reshape(data_arr,(image_msg_dict['height'],image_msg_dict['width'],num_channel))
 
-    if desire_encoding == 'bgr8':
+    #now we convert it
+    if desire_encoding == 'bgr8' and image_msg_dict['encoding'] == 'bgra8':
         data_arr = data_arr[:,:,0:3] 
+    
+    return data_arr, image_msg_dict['header']
 
-    return data_arr
 
-# def numpy_to_image(image_numpy):
-#     """
-#     Convert a numpy array to sensor_msgs/Image (in dict form)
-#     only works with bgra8
-#     """
-#     if image_msg_dict['encoding'] != 'bgra8':
-#         raise NotImplementedError("Unable to convert other image besides bgra8")
+def numpy_to_image(image_numpy, encoding_name="bgr8", header=None):
+    """
+    Convert a numpy array to sensor_msgs/Image (in dict form)
+    only works with bgra8
+    """
 
-#     buffer = base64.b64decode(image_msg_dict['data'])
-#     data_arr = np.frombuffer(buffer, np.uint8)
-#     data_arr = np.reshape(data_arr,(image_msg_dict['height'],image_msg_dict['width'],4))
+    image_dict = dict()
 
-#     if desire_encoding == 'bgr8':
-#         data_arr = data_arr[:,:,0:3] 
-#     
-#     return data_arr
+    #the header
+    image_dict['header'] = header
+
+    image_shape = np.shape(image_numpy)
+    image_dict['width'] = image_shape[1]
+    image_dict['height'] = image_shape[0]
+    image_num_channels = image_shape[2]
+    #put in the data
+    #the copy here will make the image to have a continous buffer in the memory
+    buffer = np.getbuffer(image_numpy.copy()) 
+    image_dict['data'] = base64.b64encode(buffer)
+    image_dict['encoding'] = encoding_name
+    image_dict['is_bigendian'] = 0
+    image_dict['step'] = image_shape[1] * image_shape[2]
+    return image_dict
--- test/test_roslibpy_conversion.py
+++ test/test_roslibpy_conversion.py
@@ -1,16 +1,18 @@
 import roslibpy
 
-from alloy.ros.roslibpy import image_to_numpy
+from alloy.ros import roslibpy as rlp
 
 def main():
     ros = roslibpy.Ros(host='localhost', port=9090)
     listener = roslibpy.Topic(ros, '/cameras/left_hand_camera/image', 'sensor_msgs/Image')
+    publisher = roslibpy.Topic(ros, '/image_output', 'sensor_msgs/Image')
 
     def start_listening():
         listener.subscribe(receive_message)
 
     def receive_message(message):
-        image_to_numpy(message)
+        data, header = rlp.image_to_numpy(message, 'bgr8')
+        publisher.publish(rlp.numpy_to_image(data, header=header))
 
     ros.on_ready(start_listening)
     ros.run_forever()
